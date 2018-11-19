a5c28bb3a793616ba41e2cf8add5d7a3770df542
Nathael Arki <nathael.arki@supinternet.fr>
2018-11-19 22:24:40 +0100

    added nav layout + fixed url mandatory in pasteAsMarkdown

--- pasteAsMarkdown/models.py
+++ pasteAsMarkdown/models.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 
 class Markdown(models.Model):
     markdownText = models.TextField()
-    url = models.CharField(max_length=40, unique=True)
+    url = models.CharField(max_length=40, unique=True, blank=True)
 
     def __str__(self):
         return self.markdownText
